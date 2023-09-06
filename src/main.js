354a6be8b7c849e071fe7f8c54385e6bb47355d1
IMyself <365172043@qq.com>
2023-09-06 19:53:43 +0800

    修改样式冲突

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui';
 import 'assets/css/tailwindcss.css'
+import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import { createPinia, PiniaVuePlugin } from 'pinia'
 Vue.use(ElementUI)
